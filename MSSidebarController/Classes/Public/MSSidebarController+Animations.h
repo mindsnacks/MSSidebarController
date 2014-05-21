80b9f9ba98a793df8aba0fe5c8da75486161fc7d
Chris Streeter <chris@chrisstreeter.com>
2014-05-21 14:34:00 -0700

    Wrap comment lines at 80 characters.

--- MSSidebarController/Classes/Public/MSSidebarController+Animations.h
+++ MSSidebarController/Classes/Public/MSSidebarController+Animations.h
@@ -11,23 +11,34 @@
 @interface MSSidebarController ()
 
 /**
- * Set the preferred status bar style. Setting this causes the view controller's `preferredStatusBarStyle` method to return the value given for `style`. To cause this view controller to use the value for the current view controller, use `setCurrentStatusBarStyleWithCurrentViewController`.
+ * Set the preferred status bar style. Setting this causes the view controller's
+ * `preferredStatusBarStyle` method to return the value given for `style`. To
+ * cause this view controller to use the value for the current view controller,
+ * use `setCurrentStatusBarStyleWithCurrentViewController`.
  *
  * @discussion This method internally calls `setNeedsStatusBarAppearanceUpdate`.
  *
- * @param style UIStatusBarStyle is the status bar style this view controller prefers.
+ * @param style UIStatusBarStyle is the status bar style this view controller
+ *      prefers.
  */
 - (void)setCurrentStatusBarStyle:(UIStatusBarStyle)style;
 
 /**
- * Use the result of calling `preferredStatusBarStyle` on the `currentViewController` as the result of calling `preferredStatusBarStyle` on this view controller. This overrides any call to `setCurrentStatusBarStyle:`.
+ * Use the result of calling `preferredStatusBarStyle` on the
+ * `currentViewController` as the result of calling `preferredStatusBarStyle` on
+ * this view controller. This overrides any call to `setCurrentStatusBarStyle:`.
  *
- * @discussion This method internally calls `setNeedsStatusBarAppearanceUpdate`. This should be called when the `currentViewController` changes, typically from inside of the animator blocks.
+ * @discussion This method internally calls `setNeedsStatusBarAppearanceUpdate`.
+ *      This should be called when the `currentViewController` changes,
+ *      typically from inside of the animator blocks.
  */
 - (void)setCurrentStatusBarStyleWithCurrentViewController;
 
 /**
- * Set the preferred status bar visibility. Setting this causes the view controller's `prefersStatusBarHidden` method to return the value given for `hidden`. To cause this view controller to use the value for the current view controller, use `setStatusBarVisibilityWithCurrentViewController`.
+ * Set the preferred status bar visibility. Setting this causes the view
+ * controller's `prefersStatusBarHidden` method to return the value given for
+ * `hidden`. To cause this view controller to use the value for the current view
+ * controller, use `setStatusBarVisibilityWithCurrentViewController`.
  *
  * @discussion This method internally calls `setNeedsStatusBarAppearanceUpdate`.
  *
@@ -36,9 +47,13 @@
 - (void)setStatusBarIsHidden:(BOOL)hidden;
 
 /**
- * Use the result of calling `prefersStatusBarHidden` on the `currentViewController` as the result of calling `prefersStatusBarHidden` on this view controller. This overrides any call to `setStatusBarIsHidden:`.
+ * Use the result of calling `prefersStatusBarHidden` on the
+ * `currentViewController` as the result of calling `prefersStatusBarHidden` on
+ * this view controller. This overrides any call to `setStatusBarIsHidden:`.
  *
- * @discussion This method internally calls `setNeedsStatusBarAppearanceUpdate`. This should be called when the `currentViewController` changes, typically from inside of the animator blocks.
+ * @discussion This method internally calls `setNeedsStatusBarAppearanceUpdate`.
+ *      This should be called when the `currentViewController` changes,
+ *      typically from inside of the animator blocks.
  */
 - (void)setStatusBarVisibilityWithCurrentViewController;
 
